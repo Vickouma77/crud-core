dc99057b0928584eb9d3b0d660c61c9fc20b6512
Vickouma77 <Vickprestone20@gmail.com>
2025-06-09 19:49:09 +0300

    feat: implement post index component with CRUD functionality and routing

--- app/src/app/post/index/index.ts
+++ app/src/app/post/index/index.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post-service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './index.html',
   styleUrl: './index.css'
 })
 export class Index {
+  posts: Post[] = [];
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.loadPosts();
+  }
+
+  deletePost(id: number){
+    if(confirm("Are you sure to remove this post?")){
+      this.postService.deletePost(id).subscribe(()=>{
+          this.loadPosts();
+      });
+    }
+  }
+
+  loadPosts() {
+    this.postService.getPosts().subscribe((data: Post[]) => {
+      this.posts = data
+    })
+  }
 }
